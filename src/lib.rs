e2855b1b258f32d5b43fdae73e0aacae44566dbc
Rob Dimsdale-Zucker <robert.dimsdale@gmail.com>
2020-06-25 09:18:22 -0400

    Add benchmark test.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+#![feature(test)]
+
+extern crate test;
+
 use itertools::iproduct;
 use itertools::Itertools;
 use std::fmt;
@@ -194,4 +198,66 @@ fn find_word_in_direction(
     }
 
     None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_solve_square(b: &mut Bencher) {
+        let square = Square::new(&vec![
+            vec!['t', 'o', 'p'],
+            vec!['a', 'e', 'z'],
+            vec!['n', 'o', 'w'],
+        ]);
+        let words = ["now", "pen", "tan", "top"];
+
+        b.iter(|| {
+            let found_words = solve_square(&square, &words);
+
+            assert_eq!(found_words.len(), 4);
+
+            assert_eq!(
+                found_words[0],
+                WordLocation {
+                    word: "now".to_string(),
+                    start_cell: Cell { row: 2, col: 0 },
+                    end_cell: Cell { row: 2, col: 2 },
+                    direction: Direction::Right,
+                }
+            );
+
+            assert_eq!(
+                found_words[1],
+                WordLocation {
+                    word: "pen".to_string(),
+                    start_cell: Cell { row: 0, col: 2 },
+                    end_cell: Cell { row: 2, col: 0 },
+                    direction: Direction::DownLeft,
+                }
+            );
+
+            assert_eq!(
+                found_words[2],
+                WordLocation {
+                    word: "tan".to_string(),
+                    start_cell: Cell { row: 0, col: 0 },
+                    end_cell: Cell { row: 2, col: 0 },
+                    direction: Direction::Down,
+                }
+            );
+
+            assert_eq!(
+                found_words[3],
+                WordLocation {
+                    word: "top".to_string(),
+                    start_cell: Cell { row: 0, col: 0 },
+                    end_cell: Cell { row: 0, col: 2 },
+                    direction: Direction::Right,
+                }
+            );
+        });
+    }
 }
