bcb4adb25a6f3982ca499cdea8639a559e52b65b
Rob Dimsdale-Zucker <robert.dimsdale@gmail.com>
2020-06-29 17:20:10 -0400

    Allow generation size to be provided via args.
    - Using the newly-added clap library.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
+use clap::{App, Arg};
 use wordsearch::*;
 
+const ROWS_SIZE_ARG: &str = "rows";
+const COLS_SIZE_ARG: &str = "cols";
+
 fn main() {
+    let matches = App::new("Wordsearch")
+        .version("0.1.0")
+        .author("Rob Dimsdale-Zucker")
+        .about("Generates and solves wordsearches")
+        .arg(
+            Arg::with_name(ROWS_SIZE_ARG)
+                .long(ROWS_SIZE_ARG)
+                .help("Number of rows in generated wordsearch.")
+                .takes_value(true)
+                .value_name("ROWS"),
+        )
+        .arg(
+            Arg::with_name(COLS_SIZE_ARG)
+                .long(COLS_SIZE_ARG)
+                .help("Number of columns in generated wordsearch.")
+                .takes_value(true)
+                .value_name("COLS"),
+        )
+        .get_matches();
+
     let words = [
         "anxious",
         "blossom",
@@ -22,7 +46,18 @@ fn main() {
         "stress",
         "vivacious",
     ];
-    let grid = generate_grid(15, 15, &words).unwrap();
+
+    let rows = match matches.value_of(ROWS_SIZE_ARG) {
+        None => 15,
+        Some(v) => v.parse().unwrap(),
+    };
+
+    let cols = match matches.value_of(COLS_SIZE_ARG) {
+        None => 15,
+        Some(v) => v.parse().unwrap(),
+    };
+
+    let grid = generate_grid(rows, cols, &words).unwrap();
 
     println!("words: {:?}", words);
     print!("{}", grid);
